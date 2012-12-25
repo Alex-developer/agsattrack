210edda6f8ce29405d1e6a680c51bc839210e5ab
Alex Greenland <alex.greenland@btinternet.com>
2012-12-25 21:51:12 +0000

    Commented class

--- js/classes/observer.js
+++ js/classes/observer.js
@@ -12,21 +12,29 @@ Copyright 2012 Alex Greenland
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    See the License for the specific language governing permissions and
    limitations under the License.
- */ 
-var AGOBSERVER = function() {
+ */
+;var AGOBSERVER = function() {
     'use strict';
     
-    var _lat = 0;
-    var _lon = 0;
-    var _alt = 0;
+    var _lat = 0; // The observers latitude
+    var _lon = 0; // The observers longitude
+    var _alt = 0; // The observers altitude
     
-	var _name = 'Home';
-	var _ready = false;
+	var _name = 'Home'; // The observers name
+	var _ready = false; // Flag indicating if we have geo location data
 
     
 	return {
 	
 		init : function() {
+            /**
+            * If the browser has geolocation capabilitis then get the users current position.
+            * NOTE: This will not be accurate but will give a rough idea for the users location. Also if the
+            * user cancels the request then we just use 0,0,0 - the defaults.
+            * 
+            * Once the geolocation data is available, or fails an event is fired to notify other area of the app
+            * that an observer is now available.
+            */
 			var that = this;
 		    if ('geolocation' in navigator) {
 		        navigator.geolocation.getCurrentPosition(
@@ -59,6 +67,9 @@ var AGOBSERVER = function() {
 			return _ready;
 		},
 		
+        /**
+        * Getter and setter for the observers name
+        */
 		setName: function(name) {
 			_name = name;
 		},
@@ -66,26 +77,39 @@ var AGOBSERVER = function() {
 			return _name;
 		},
 
+        /**
+        * Getter and setter for observers latitude.
+        */
 		setLat: function(lat) {
 			_lat = lat;
 		},		
 		getLat: function() {
 			return _lat;
 		},
-		
+
+        /**
+        * Getter and setter for observers longitude.
+        */		
 		setLon: function(lon) {
 			_lon = lon;
 		},		
 		getLon: function() {
 			return _lon;
 		},
-		
+
+        /**
+        * Getter and setter for observers altitude.
+        */
 		setAlt: function(alt) {
 			_alt = alt;
 		},		
 		getAlt: function() {
 			return _alt;
 		},
+        
+        /**
+        * Return a string representation of the observer
+        */
 		toString : function() {
 			return 'name=' + _name + ', lat=' + _lat + ', lon=' + _lon;
 		}
