0dc70af1bcf92332d169d458335b6d99189a508a
Alex <alex.greenland@btinternet.com>
2013-01-17 22:33:14 +0000

    Changed htnl list to a grid

--- js/classes/views/listview.js
+++ js/classes/views/listview.js
@@ -1,5 +1,5 @@
 /*
-Copyright 2012 Alex Greenland
+Copyright 2013 Alex Greenland
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -13,19 +13,22 @@ Copyright 2012 Alex Greenland
    See the License for the specific language governing permissions and
    limitations under the License.
  */ 
+ 
+ /**
+ * The list view display. Contains a grid listing all of the satellites
+ */
 var AGLISTVIEW = function() {
 	'use strict';
 	
-	var _render = false;
-	var _calcAoS = true;
-	var _contextMenu = null;
-    
+	var _render = false;            // Flag to indicate this view is active and needs rendering
+    var _gridPolulated = false;     // Flag to indicate that the grid is populated. Used to call populate or update
+       
 	/**
 	 * Listen for an event telling us a new set of elements were loaded
 	 */
-	jQuery(document).bind('agsattrack.tlesloaded', function(event, group) {
+	jQuery(document).bind('agsattrack.satsselectedcomplete', function(event, group) {
 		if (_render) {
-			buildTable();
+            clearSatelliteGrid()
 		}
 	});
 	
@@ -35,25 +38,13 @@ var AGLISTVIEW = function() {
 	 */
 	jQuery(document).bind('agsattrack.updatesatdata', function(event) {
 		if (_render) {
-			buildTable();
+            if (!_gridPolulated) {
+                populateSatelliteGrid();
+            } else {
+                updateSatelliteGrid();
+            }
 		}
 	});
-
-    jQuery(document).bind('agsattrack.showevents', function(state) {
-        AGSETTINGS.setCalculateEvents(state);
-    });    
-    
-    
-	/**
-	 * Listen for event telling us to stop or start calculating AoS times. This event
-	 * will normally be triggered by the ribbon or settings being changed.
-	 */
-	jQuery(document).bind('agsattrack.showaos', function(event, state) {
-		_calcAoS = state;
-		if (_render) {
-			buildTable();
-		}
-	})	
 	
     /**
     * Reset the view
@@ -65,116 +56,177 @@ var AGLISTVIEW = function() {
                 }
             });        
 	
-	/**
-	 * Rebuild the table of satellites
-	 */
-	function buildTable() {
-		jQuery('#listviewdata').html('');
-		
-		var satellites = AGSatTrack.getDisplaying();
-		
-		var html = '';
-		jQuery.each(satellites, function(index, satellite) {
-			var rowCSSArray = ['listviewsat'];
-			if (_calcAoS) {
-				var rowCSS = '';
-				if (parseInt(satellite.get('elevation').toFixed(0),10) > parseInt(AGSETTINGS.getAosEl(),10)) {
-					rowCSSArray.push('green');
-				}				
-			}
-			if (satellite.getSelected()) {
-				rowCSSArray.push('highlighted');
-			}				
-			rowCSS = 'class="' + rowCSSArray.join(' ') + '"';
+    /**
+    * Clear the grid of all data
+    */
+    function clearSatelliteGrid() {
+        var data = [];
+        jQuery('#sat-list-grid').datagrid('loadData',data);
+        _gridPolulated = false;      
+    }
 
-			
-			html += '<tr ' + rowCSS + ' ' + 'id="' + satellite.getCatalogNumber() + '">';
-			html += '<td>' + satellite.getName() + '</td>';
-            html += '<td>' + satellite.get('type') + '</td>';
-			html += '<td>' + satellite.get('visibility').substring(0,3) + '</td>';
-			html += '<td align="right">' + satellite.get('azimuth').toFixed(2) + '&deg;</td>';
-			html += '<td align="right">' + satellite.get('elevation').toFixed(2) + '&deg;</td>';
-			html += '<td align="right">' + AGUTIL.convertDecDegLat(satellite.get('latitude')) + '&deg;</td>';				
-			html += '<td align="right">' + AGUTIL.convertDecDegLon(satellite.get('longitude')) + '</td>';	
-			html += '<td align="right">' + satellite.get('altitude').toFixed(0) + '</td>';
-            html += '<td align="right">' + satellite.get('velocity').toFixed(0) + '</td>';
-			
+    /**
+    * Do the initial population of the grid
+    */
+    function populateSatelliteGrid() {
+        var satellites = AGSatTrack.getDisplaying();
+        if (satellites.length > 0) {
+            var data = [];
+            var theNextEvent = '';
+            jQuery.each(satellites, function(index, satellite) {
+                if (AGSETTINGS.getCalculateEvents()) {
+                    if (satellite.isGeostationary()) {
+                        theNextEvent = 'Geostationary';
+                    } else {
+                        theNextEvent = satellite.getNextEvent();
+                    }
+                } else {
+                    theNextEvent = 'Disabled';
+                }            
+                data.push({
+                    name: satellite.getName(),
+                    type: satellite.get('type'),
+                    viz: satellite.get('visibility').substring(0,3),
+                    az: satellite.get('azimuth').toFixed(2) + '&deg;',
+                    el: satellite.get('elevation').toFixed(2) + '&deg;',
+                    lat: AGUTIL.convertDecDegLat(satellite.get('latitude')),
+                    lon: AGUTIL.convertDecDegLon(satellite.get('longitude')),
+                    alt: satellite.get('altitude').toFixed(0),
+                    vel: satellite.get('velocity').toFixed(0),
+                    nextevent: theNextEvent,
+                    catalognumber: satellite.getCatalogNumber()
+                }); 
+        
+            });
+
+            jQuery('#sat-list-grid').datagrid('loadData',data);
+            _gridPolulated = true; 
+        } 
+             
+    }
+    
+    /**
+    * Update the grid. This is faster than re populating the grid
+    */
+    function updateSatelliteGrid() {
+        var catalogNumber;
+        var satellite;
+        var dataGrid = jQuery('#sat-list-grid');
+        var theNextEvent = '';
+        var rows = dataGrid.datagrid('getRows');
+        
+        for (var i=0; i<rows.length; i++) {
+            catalogNumber = rows[i].catalognumber;
+            satellite = AGSatTrack.getSatelliteByName(catalogNumber);
             if (AGSETTINGS.getCalculateEvents()) {
                 if (satellite.isGeostationary()) {
-                    html += '<td>Geostationary</td>';
+                    theNextEvent = 'Geostationary';
                 } else {
-                    html += '<td>' + satellite.getNextEvent() + '</td>';
+                    theNextEvent = satellite.getNextEvent();
                 }
             } else {
-                html += '<td>Disabled</td>';
-            }
-            
-            /*
-            if (_calcAoS && satellite.get('next_aos')) {
-                
-                if (satellite.aosHappens()) {
-				    html += '<td>' + AGUTIL.shortdate(satellite.get('next_aos')) + '</td>';
-                } else {
-                     html += '<td>Never</td>';   
+                theNextEvent = 'Disabled';
+            }                   
+            dataGrid.datagrid('updateRow',{
+                index: i,
+                row: {
+                    viz: satellite.get('visibility').substring(0,3),
+                    az: satellite.get('azimuth').toFixed(2) + '&deg;',
+                    el: satellite.get('elevation').toFixed(2) + '&deg;',
+                    lat: AGUTIL.convertDecDegLat(satellite.get('latitude')),
+                    lon: AGUTIL.convertDecDegLon(satellite.get('longitude')),
+                    alt: satellite.get('altitude').toFixed(0),
+                    vel: satellite.get('velocity').toFixed(0),
+                    nextevent: theNextEvent
                 }
-			} else {
-				html += '<td>N/A</td>';
-			}
-            */
-            
-			html += '</tr>';
-		});
-		jQuery('#listviewdata').html(html);
-		jQuery('.listviewsat').on('click',{}, function(e){
-            var selected = jQuery(this).attr('id');
-			jQuery(document).trigger('agsattrack.satclicked', {catalogNumber: selected});			
-		});		
-	}
-	
-	/*
-    _contextMenu = $("#jqxListViewMenu").jqxMenu({ width: '120px', height: '140px', autoOpenPopup: false, mode: 'popup' });
-    jQuery(document).bind('contextmenu', function (e) {
-        return false;
-    });
-    jQuery('#list').on('mousedown', function (event) {
-        var rightClick = isRightClick(event);
-        if (rightClick) {
-            var scrollTop = jQuery(window).scrollTop();
-            var scrollLeft = jQuery(window).scrollLeft();
-            _contextMenu.jqxMenu('open', parseInt(event.clientX) + 5 + scrollLeft, parseInt(event.clientY) + 5 + scrollTop);
-            return false;
+            });                
         }
-     });
-              
-    function isRightClick(event) {
-        var rightclick;
-        if (!event) var event = window.event;
-        if (event.which) rightclick = (event.which == 3);
-        else if (event.button) rightclick = (event.button == 2);
-        return rightclick;
     }
+    
+    /**
+    * Handle paging in the grid
+    *  
+    * @param {Object} data
+    * 
+    * @returns {Object}
     */
-                                
+    function pagerFilter(data){
+        if (typeof data.length == 'number' && typeof data.splice == 'function'){    // is array
+            data = {
+                total: data.length,
+                rows: data
+            }
+        }
+        var dg = $(this);
+        var opts = dg.datagrid('options');
+        var pager = dg.datagrid('getPager');
+        pager.pagination({
+            onSelectPage:function(pageNum, pageSize){
+                opts.pageNumber = pageNum;
+                opts.pageSize = pageSize;
+                pager.pagination('refresh',{
+                    pageNumber:pageNum,
+                    pageSize:pageSize
+                });
+                dg.datagrid('loadData',data);
+            }
+        });
+        if (!data.originalRows){
+            data.originalRows = (data.rows);
+        }
+        var start = (opts.pageNumber-1)*parseInt(opts.pageSize);
+        var end = start + parseInt(opts.pageSize);
+        data.rows = (data.originalRows.slice(start, end));
+        return data;
+    } 
+                                            
 	return {
+        /**
+        * Start to render the views
+        */
 		startRender : function() {
-			_render = true;
-			buildTable();
+            populateSatelliteGrid();
+            _render = true;
 		},
-		
+
+        /**
+        * Stop rendering rhe view
+        */
 		stopRender : function() {
 			_render = false;			
 		},
-		
+
+        /**
+        * Initialise the view. Sets up the data grid
+        */
 		init : function() {
-            
-		},
-        
-        tlesLoaded : function() {
-            var totalTles = AGSatTrack.getTles().getCount();
-            if (totalTles > 100) {
-                AGSETTINGS.setCalculateEvents(false);
-                jQuery('#list-view-show-events').setState(false);                  
-            }            
-        }
+            jQuery('#sat-list-grid').datagrid({  
+                rownumbers:true,
+                autoRowHeight:false, 
+                pagination:true, 
+                pageSize:20, 
+                fitColumns: true, 
+                fit:true,  
+                loadFilter: pagerFilter,
+                columns:[[  
+                    {field:'ck',checkbox:true},
+                    {field:'name',title:'Name',width:200},  
+                    {field:'type',title:'Type',width:55},  
+                    {field:'viz',title:'Viz',width:55},  
+                    {field:'az',title:'Azimuth',width:70},  
+                    {field:'lat',title:'Latitude',width:95,align:'right'},  
+                    {field:'lon',title:'Longitude',width:95,align:'right'},  
+                    {field:'alt',title:'Altitude',width:60,align:'right'},  
+                    {field:'vel',title:'Velocity',width:55,align:'right'},  
+                    {field:'nextevent',title:'Next Event',width:150,align:'right'}
+         
+                ]]  
+            });                            
+            jQuery('#sat-list-grid').datagrid({
+                onClickRow: function(index,data){
+                    jQuery(document).trigger('agsattrack.satclicked', {catalogNumber: data.catalognumber});
+                }
+            });           
+		}
 	}
 }
